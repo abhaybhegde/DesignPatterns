368accc6c661ecccea8a85c267602ece0e40e514
abhay <abhaybhegde@gmail.com>
2017-10-10 07:36:47 +0530

    Added Producer logic

--- src/producer_consumer/Producer.java
+++ src/producer_consumer/Producer.java
@@ -3,6 +3,8 @@ package producer_consumer;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.apache.log4j.Logger;
+
 /* A classical producer-consumer design approach.
  * 
  * In producer-consumer design approach, we have two different entities using a common shared resource.
@@ -23,10 +25,37 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class Producer {
 	
+	final static Logger logger = Logger.getLogger(Producer.class);
+	
 	public Record mRecord;
 	
-	public BlockingQueue<Record> mQueue = new LinkedBlockingQueue<>();
+	public boolean addData(Record record) {
+		
+		boolean isAddSuccess = false;
+		
+		BlockingQueue<Record> mQueue = new LinkedBlockingQueue<>();
+		
+		try {
+			isAddSuccess =  mQueue.offer(record);
+		}catch (Exception ex) {
+			logger.error("Exception caught while adding element to shared queue." + ex.getMessage());
+		}
+		
+		return isAddSuccess;
+	}
 	
+
+	
+	public static void main(String[] args) {
+		
+		Record record = new Record();
+		record.populateData("Abhay", "Hegde", 4500.00, "Computer Science");
+		Producer producer = new Producer();
+		if (producer.addData(record)) {
+			logger.debug("Record added successfully to queue.");
+		}
+		
+	}
 	
 
 
